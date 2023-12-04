ca7497ac5d25913025061177b3700f6081478dc4
AygulZeynalova <aygl.zeynalova@mail.ru>
2023-12-04 22:37:12 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task 1
+            string name = "Aygul";
+            string surname = "Zeynalova";
+            string fullname = name + " " + surname;
+            Console.WriteLine(fullname);
+
+
+            //task 2
+            int a = 3;
+            int b = 6;
+            int sum = a + b;
+            Console.WriteLine(sum);
+
+
+            //task 3
+
+            int a1 = 8;
+            int b1 = 19;
+            int sum1 = a1 - b1;
+            Console.WriteLine(sum1);
+
+            //task 4
+
+            int a2 = 20;
+            int b2 = 30;
+            int sum2 = a2 * b2;
+            int multi = sum2 * sum2;
+            Console.WriteLine(multi);
+
+            //task 5 
+
+            int a3 = 90;
+            int b3 = 30;
+            int sum3 = a3 / b3;
+            int total = sum3 * sum3;
+            Console.WriteLine(total);
+        }
+    }
+}
